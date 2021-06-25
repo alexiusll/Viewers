6d05616503f15b99fb5466f8d54c5172cf6b7396
Alexiusll <linkenzone@163.com>
2021-06-25 17:35:15 +0800

    新加注释

--- platform/viewer/babel.config.js
+++ platform/viewer/babel.config.js
@@ -1,1 +1,6 @@
-module.exports = require("../../babel.config.js");
+/*
+ * @Author: linkenzone
+ * @Date: 2021-06-15 17:31:13
+ * @Descripttion: Do not edit
+ */
+module.exports = require('../../babel.config.js');
